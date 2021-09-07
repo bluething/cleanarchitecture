9a42efed260bae04647e462e8ff9047dced0e9f9
bluething <habib.gunadarma@gmail.com>
2021-09-07 22:18:47 +0700

    Add infrastructure mysql module

---
+++ hexagonalarchitecture/codekata/product/infrastructure/mysql-persistence/src/main/java/io/github/bluething/cleanarchitecture/hexagonalarchitecture/product/infrastructure/mysqlpersistence/MysqlPersistenceApplication.java
@@ -0,0 +1,13 @@
+package io.github.bluething.cleanarchitecture.hexagonalarchitecture.product.infrastructure.mysqlpersistence;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MysqlPersistenceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MysqlPersistenceApplication.class, args);
+    }
+
+}
---
+++ hexagonalarchitecture/codekata/product/infrastructure/mysql-persistence/src/test/java/io/github/bluething/cleanarchitecture/hexagonalarchitecture/product/infrastructure/mysqlpersistence/MysqlPersistenceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.bluething.cleanarchitecture.hexagonalarchitecture.product.infrastructure.mysqlpersistence;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MysqlPersistenceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
