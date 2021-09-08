1d9647e56eb0eed880fbfda15eadf1683212f563
bluething <habib.gunadarma@gmail.com>
2021-09-08 14:10:53 +0700

    Failing test

--- hexagonalarchitecture/codekata/product/domain/src/test/java/io/github/bluething/cleanarchitecture/hexagonalarchitecture/product/domain/api/ProductServiceTest.java
+++ hexagonalarchitecture/codekata/product/domain/src/test/java/io/github/bluething/cleanarchitecture/hexagonalarchitecture/product/domain/api/ProductServiceTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.UUID;
@@ -18,16 +19,28 @@ class ProductServiceTest {
     private ProductRepository repository;
     private ProductService service;
     private Product product;
+    private UUID id;
+    private String code;
 
     @BeforeEach
     private void setupEach() {
         service = new ProductServiceImpl(repository);
-        product = new Product(UUID.randomUUID(), "123", "asd");
+        id = UUID.randomUUID();
+        code = "123";
+        product = new Product(id, "123", "asd");
     }
 
     @Test
     public void createProductThrowsIllegalArgumentExceptionWhenProductIdIsEmpty() {
         assertThrows(IllegalArgumentException.class,
                 () -> service.createProduct(new Product(null, "", "")));
+    }
+
+    @Test
+    public void createProductSuccessWhenThereAreNoProductWithSameId() {
+        Mockito.when(repository.findProductByCode(code)).thenReturn(null);
+        Mockito.when(repository.addProduct(product)).thenReturn(product);
+
+        assertEquals(product, service.createProduct(product));
     }
 }
