3d70957dbe33cad3864bd40a0e14163f7eb055d5
bluething <habib.gunadarma@gmail.com>
2021-09-09 17:02:46 +0700

    Fix import

--- hexagonalarchitecture/codekata/buckpal/common/src/main/java/io/github/bluething/cleanarchitecture/hexagonalarchitecture/buckpal/common/SelfValidating.java
+++ hexagonalarchitecture/codekata/buckpal/common/src/main/java/io/github/bluething/cleanarchitecture/hexagonalarchitecture/buckpal/common/SelfValidating.java
@@ -1,5 +1,8 @@
 package io.github.bluething.cleanarchitecture.hexagonalarchitecture.buckpal.common;
 
+import javax.validation.*;
+import java.util.Set;
+
 public abstract class SelfValidating<T> {
     private Validator validator;
 
