9695d8742e5456ef186f6762a3c6e5f9f7f5de38
bluething <habib.gunadarma@gmail.com>
2021-09-09 17:03:13 +0700

    Add adapter persistence

---
+++ hexagonalarchitecture/codekata/buckpal/adapters/persistence/src/test/java/io/github/bluething/cleanarchitecture/hexagonalarchitecture/buckpal/adapters/persistence/PersistenceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.bluething.cleanarchitecture.hexagonalarchitecture.buckpal.adapters.persistence;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersistenceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
