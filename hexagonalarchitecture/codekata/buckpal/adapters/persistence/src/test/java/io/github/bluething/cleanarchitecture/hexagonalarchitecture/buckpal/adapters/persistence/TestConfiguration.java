63ece384038c45ac9553114201754e35a67bc5df
bluething <habib.gunadarma@gmail.com>
2021-09-20 16:27:45 +0700

    Add test configuration

---
+++ hexagonalarchitecture/codekata/buckpal/adapters/persistence/src/test/java/io/github/bluething/cleanarchitecture/hexagonalarchitecture/buckpal/adapters/persistence/TestConfiguration.java
@@ -0,0 +1,11 @@
+package io.github.bluething.cleanarchitecture.hexagonalarchitecture.buckpal.adapters.persistence;
+
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootConfiguration
+@ComponentScan("io.github.bluething.cleanarchitecture.hexagonalarchitecture.buckpal.adapters.persistence")
+@EnableAutoConfiguration
+public class TestConfiguration {
+}
