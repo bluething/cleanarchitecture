8ce035e8b0ccdd84fdc112819a403c119876464b
bluething <habib.gunadarma@gmail.com>
2021-09-20 15:44:36 +0700

    Add test configuration for WebMvcTest

---
+++ hexagonalarchitecture/codekata/buckpal/adapters/web/src/test/java/io/github/bluething/cleanarchitecture/hexagonalarchitecture/buckpal/adapter/web/TestConfiguration.java
@@ -0,0 +1,11 @@
+package io.github.bluething.cleanarchitecture.hexagonalarchitecture.buckpal.adapter.web;
+
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootConfiguration
+@ComponentScan("io.github.bluething.cleanarchitecture.hexagonalarchitecture.buckpal")
+@EnableAutoConfiguration
+public class TestConfiguration {
+}
