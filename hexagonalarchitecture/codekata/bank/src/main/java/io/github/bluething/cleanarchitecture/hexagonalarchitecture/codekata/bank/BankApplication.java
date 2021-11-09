5bf3512e07000eea3bf67eb5619ea17fe896ef65
bluething <habib.gunadarma@gmail.com>
2021-11-09 17:05:25 +0700

    Add bank module

---
+++ hexagonalarchitecture/codekata/bank/src/main/java/io/github/bluething/cleanarchitecture/hexagonalarchitecture/codekata/bank/BankApplication.java
@@ -0,0 +1,13 @@
+package io.github.bluething.cleanarchitecture.hexagonalarchitecture.codekata.bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BankApplication.class, args);
+    }
+
+}
